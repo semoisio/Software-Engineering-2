25ba12c92e685bcac2afd540fdec77261f77e323
Severi Moisio <semoisio@uef.fi>
2021-02-03 11:01:03 +0200

    Tehty spa rakenne

--- kieltenopetussovellus/src/App.js
+++ kieltenopetussovellus/src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Switch, Route, BrowserRouter as Router} from 'react-router-dom'
+import HomePage from './components/homePage/HomePage'
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-        
+        <Router>
+            <Switch>
+                <Route exact path="/" component={HomePage} />
+            </Switch>
+        </Router>
     </div>
   );
 }
