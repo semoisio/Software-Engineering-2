261b54caeb0854f4c69694ae667e1221ddbc0a00
Wokki <ekselinen@msn.com>
2021-02-12 12:35:25 +0200

    Lisäilty testit signin näkymälle

---
+++ kieltenopetussovellus/src/tests/SignIn.test.js
@@ -0,0 +1,31 @@
+import { render, screen,cleanup, fireEvent,waitFor } from '@testing-library/react';
+import App from '../App';
+
+afterEach(cleanup);
+
+test('renders signin component after clicking signin button', async () => {
+    render(<App />);
+
+    fireEvent.click(screen.getByTestId('navbarBtnLink1'));
+    
+    const element = screen.getByTestId("signincontainer")
+    
+    expect(element).toBeInTheDocument();
+  });
+
+
+  test('Homepage isnt rendered anymore', async () => {
+
+    render(<App />);
+    const element = screen.queryByTestId("homepage");
+    expect(element).toBeNull();
+  });
+
+  test('Homepage renders again', async () => {
+
+    render(<App />);
+
+    fireEvent.click(screen.getByTestId('navbarLogo'));
+    const element = screen.getByTestId("homepage");
+    expect(element).toBeInTheDocument();
+  });
