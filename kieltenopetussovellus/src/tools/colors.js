9bb8c96a3f3d75a81da8ebb6df679810ba5e6988
Anna R <raappana.anna@gmail.com>
2021-02-23 19:19:57 +0200

    Päivitetty colors.js ja index.css yhteinen ulkoasu

--- kieltenopetussovellus/src/tools/colors.js
+++ kieltenopetussovellus/src/tools/colors.js
@@ -1,1 +1,55 @@
-export const background = "linear-gradient(to top, #f0ffef, #e5fae3, #daf5d8, #cef0cc, #c3ebc1);";
+//YLÄPALKKI:
+//Taustaväri yläpalkki (gradient)
+export const NAVbg = "linear-gradient(to right top, #08ffbf, #07ecb5, #0ed9ab, #16c6a0, #1eb495)";
+//Nappi hover & active (tummempi vihreä)
+export const NAVbtn = "#1EB495";
+//Nappi active alleviivaus (oranssi)
+export const NAVline = "#FFC67C";
+//Teksti (valkea)
+export const NAVtext = "#FFFFFF";
+//Tesktin varjostus (harmaa)
+export const NAVshadow = "#626262";
+
+
+//SISÄLTÖ
+//Taustaväri sisältö (valkea)
+export const background = "#FBFBFB";
+//Teksti (musta)
+export const CONtext = "#000000";
+//Otsikko (tummepi vihreä)
+export const CONh1 = "#1EB495";
+//Tekstikentän reunaviiva (vaalean harmaa)
+export const CONinput = "#BFBFBF";
+
+
+//ORANSSI NAPPI (NavLink)
+//Taustaväri (oranssi)
+export const OBbg = "#FF995E";
+//Hover (vaalea oranssi)
+export const OBhover = "#FFC67C";
+//Tekstin väri (valkea)
+export const OBtext = "#FFFFFF";
+//Tesktin varjostus (harmaa)
+export const OBshadow = "#626262";
+
+
+//PUNAINEN NAPPI (NavLink)
+//Taustaväri (punainen)
+export const RBbg = "#FE786A";
+//Hover (vaalea punainen)
+export const RBhover = "#FE8A7E";
+//Tekstin väri (valkea)
+export const RBtext = "#FFFFFF";
+//Tesktin varjostus (harmaa)
+export const RBshadow = "#626262";
+
+//FOOTER
+//Taustaväri footer (tummempi vihreä)
+export const FOObg = "#1EB495";
+//Linkin teksti footer (valkoinen)
+export const FOOlink= "#FFFFFF";
+//Tesktin varjostus (harmaa)
+export const FOOshadow = "#626262";
+//Tekstin hover (vaalean oranssi)
+export const FOOhover= "#FFC67C";
+
