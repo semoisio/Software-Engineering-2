fc0214452cd0b7415982f234830908c10eed02bb
Anna R <raappana.anna@gmail.com>
2021-03-24 07:27:05 +0200

    LanguageNavBar muotoilua fiksailtu

--- kieltenopetussovellus/src/components/languageAppPage/languageNavbar/LanguageNavbarElements.js
+++ kieltenopetussovellus/src/components/languageAppPage/languageNavbar/LanguageNavbarElements.js
@@ -1,63 +1,80 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
 
-export const LanguageNav = styled.div`
-    background: #E0FFCD;
-    height: 80px;
+export const LanguageNav = styled.nav`
+    background: linear-gradient(to right top, #08ffbf, #07ecb5, #0ed9ab, #16c6a0, #1eb495);
+    height: 85px;
     //margin-top: -80px;
     display: flex;
-    justify-content: space-around;
+    justify-content: space-between;
     align-items: center;
     font-size: 1rem;
-    position: fixed;
+    position: sticky;
     top: 0;
     width:100%;
     z-index: 10;
+    margin-right:0;
 
     @media screen and (max-width: 960px){
         transition: 0.8s all ease;
     }
-    @media screen and (max-width: 768px){
+    /* @media screen and (max-width: 768px){
         justify-content:flex-start;
-    }
+    } */
 `;
 
+//LOGO
 export const Logo = styled.div`
-    color: #000000;
+    color: white;
+    justify-self: flex-start;
+    cursor: pointer;
     font-size: 1.5rem;
+    display: flex;
+    align-items: center;
+    margin-left: 24px;
     font-weight: bold;
-    width:20%;
-    margin-left:2rem;
+    text-decoration: none;
+    //width:20%;
+    //margin-left:2rem;
 `;
-export const NavBtnWrapper = styled.div`
+
+//Yläpalkin buttonien tausta-nav
+export const NavBtnWrapper = styled.nav`
     display:flex;
-    align-items:center;
-    justify-content:space-between;
+    align-items: center;
     width:80%;
-    margin-right: 2rem;
+    height: 80px;
+    flex-direction:row;
+    justify-content: flex-end;
+    font-size: 1.2rem;
+    
 
     @media screen and (max-width: 768px){
         display:none;
     }
 `;
 
+//Yläpalkin buttonit
 export const NavBtn = styled(NavLink)`
-    border-radius: 50px;
-    background: #FFC67C;
-    white-space: nowrap;
-    padding: 10px 27px;
-    color: #FFFFFF;
-    font-size: 16px;
-    cursor: pointer;
-    transition: all 0.2s ease-in-out;
-    margin: 5px;
+    color: #fff;
+    text-shadow: 1px 1px 2px #626262;
+    display: flex;
+    align-items: center;
     text-decoration: none;
-
+    padding: 1.8rem 1rem;
+    cursor: pointer;
+    
+    &:active{
+        border-bottom: 3px solid #FFC67C;
+        background: #C3EBC1;
+    }
     &:hover{
         transition: all 0.2s ease-in-out;
-        background: #fff;
-        background: #68EDCB;
+        background: #1EB495;
     }
+    &.${props => props.activeClassName} {
+        background: #1EB495;
+  }
 `;
 
 export const MobileIcon = styled.div`
