7502dd7200a918188d3960340098263e44f0bb79
Anna R <raappana.anna@gmail.com>
2021-03-24 07:26:32 +0200

    Fiksailtu IntroPage muotoilua

--- kieltenopetussovellus/src/components/homePage/IntroPageJS.js
+++ kieltenopetussovellus/src/components/homePage/IntroPageJS.js
@@ -22,8 +22,8 @@ function IntroPageJS () {
 					<IntroLogoImg src="img/thumb.jpg" alt=""/>
 				</IntroLogoDiv>
 				<IntroText>
-					<IntroH1>Welcome to Name</IntroH1>
-					<IntroText>Lorem ipsum dolor sit amet, consectetur, adipisicing elit. Saepe, totam? Nulla, quisquam perspiciatis minima distinctio debitis tenetur ratione, blanditiis fuga!minima distinctio debitis tenetur ratione, blanditiis fuga!</IntroText>
+					<IntroH1>Welcome to LingoRec</IntroH1>
+					<IntroText>Improve your language skills by listening and recording short audio clips in LingoRec! You can listen, record and share new content every day. We welcome all learners from beginner to expert to join the flow and start practising today. </IntroText>
 				</IntroText>
 				<NavBtn>
 					<IntroButton to='/signin' href="">Sign in</IntroButton>
